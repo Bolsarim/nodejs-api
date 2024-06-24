fba666b97862c89c3170f0183dd3e7661f7f17f6
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-06-24 19:44:30 -0300

    a\

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ app.get("/", function(req, res){
     res.send("Hello World")
 })
 
-app.get("/ptbr", function(req, res){
+app.get("/pt-br", function(req, res){
     res.send("Olá, mundo")
+})
+
+app.get("/produtos", function(req, res){
+    res.send("Lista de Produtos")
 })
 
 app.listen(3000)
